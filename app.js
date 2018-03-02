4af451f61a93478ef42257cddc42078eb5c462a8
Peem Takoonsawat <mrpeem@gmail.com>
2018-03-02 14:10:11 -0800

    move stuff around

--- app.js
+++ app.js
@@ -224,10 +224,12 @@ io.sockets.on('connection', function(socket){
 
   });
 
+    socket.emit('cock', profile.cock());
+
   //sends loginStatus directly froma app.js to avoid error from asynchronicity
   socket.emit('loginStatus', profile.getLoginStatus() );
   socket.emit('cock', profile.cock());
-  
+
   console.log("app.js; loginStatus = "+profile.getLoginStatus());
 
 
