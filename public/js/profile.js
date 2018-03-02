9b65f53efaaf5e9c92ef55c5382474d793d857bc
Peem Takoonsawat <mrpeem@gmail.com>
2018-03-02 14:17:57 -0800

    more moving around

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,5 +1,12 @@
 //var socket = io.connect('http://localhost:3000');
 var socket = io.connect('https://a8-cogs120team2.herokuapp.com/');
+socket.once('cock', function(userData)
+{
+	alert(userData);
+	alert(userData.email);
+	alert(userData.loginStatus);
+});
+
 //gets loginStatus directly from app.js to avoid asynchronicity shit
 socket.once('loginStatus', function(signedIn)
 {
@@ -19,12 +26,7 @@ socket.once('loginStatus', function(signedIn)
   }
 });
 
-socket.once('cock', function(userData)
-{
-	alert(userData);
-	alert(userData.email);
-	alert(userData.loginStatus);
-});
+
 
 function logout()
 {
