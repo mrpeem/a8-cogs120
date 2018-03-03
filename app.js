e071a1061acda33815ce1a2b66f23a054de419d4
Peem Takoonsawat <mrpeem@gmail.com>
2018-03-03 03:12:39 -0800

    deleting comment; better not get called

--- app.js
+++ app.js
@@ -227,7 +227,6 @@ io.sockets.on('connection', function(socket){
 
 
   //sends loginStatus directly froma app.js to avoid error from asynchronicity
-  //socket.emit('loginStatus', profile.getLoginStatus1(), profile.getLoginStatus() );
 
   //console.log("app.js; loginStatus = "+profile.getLoginStatus());
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,4 +1,4 @@
-//var socket = io.connect('http://localhost:3000');
+  //var socket = io.connect('http://localhost:3000');
 var socket = io.connect('https://a8-cogs120team2.herokuapp.com/');
 
 
