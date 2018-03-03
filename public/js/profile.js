dd07ff36d54541cb0c050800ab3600abb4f89842
Peem Takoonsawat <mrpeem@gmail.com>
2018-03-03 02:32:06 -0800

    return undefined

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,7 +5,7 @@ var socket = io.connect('https://a8-cogs120team2.herokuapp.com/');
 //gets loginStatus directly from app.js to avoid asynchronicity shit
 socket.once('loginStatus', function(signedIn, signedIn1)
 {
-  alert("LOGIN STATUS = "+signedIn.loginStatus);
+  alert("LOGIN STATUS = "+signedIn);
   alert("cock = "+signedIn1);
   if (!signedIn) 
   {
