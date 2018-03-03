fcf0ace9b1b96ba7cb46c968045afb517f433a03
Peem Takoonsawat <mrpeem@gmail.com>
2018-03-03 03:05:07 -0800

    commented out

--- app.js
+++ app.js
@@ -227,9 +227,9 @@ io.sockets.on('connection', function(socket){
 
 
   //sends loginStatus directly froma app.js to avoid error from asynchronicity
-  socket.emit('loginStatus', profile.getLoginStatus1(), profile.getLoginStatus() );
+  //socket.emit('loginStatus', profile.getLoginStatus1(), profile.getLoginStatus() );
 
-  console.log("app.js; loginStatus = "+profile.getLoginStatus());
+  //console.log("app.js; loginStatus = "+profile.getLoginStatus());
 
 
 
